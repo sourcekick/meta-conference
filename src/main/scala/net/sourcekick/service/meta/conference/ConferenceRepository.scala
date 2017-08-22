6a8309b2939bfba6ca7b56bc1edd4691d18b3b64
sourcekick <sourcekick@gmail.com>
2017-08-22 14:02:06 +0200

    cosmetics

--- src/main/scala/net/sourcekick/service/meta/conference/ConferenceRepository.scala
+++ src/main/scala/net/sourcekick/service/meta/conference/ConferenceRepository.scala
@@ -1,9 +1,3 @@
-/**
-  * Copyright (C) Recogizer Group GmbH - All Rights Reserved
-  * Unauthorized copying of this file, via any medium is strictly prohibited
-  * Proprietary and confidential
-  * Created on 21.08.17.
-  */
 package net.sourcekick.service.meta.conference
 
 import scala.concurrent.{ExecutionContext, Future}
