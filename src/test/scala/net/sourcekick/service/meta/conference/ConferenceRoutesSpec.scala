3ea081efad09f38364b45b09087d92bdad90ca61
sourcekick <sourcekick@gmail.com>
2017-08-22 11:44:43 +0200

    skeleton for a Spec regarding the routes

--- src/test/scala/net/sourcekick/service/meta/conference/ConferenceRoutesSpec.scala
+++ src/test/scala/net/sourcekick/service/meta/conference/ConferenceRoutesSpec.scala
@@ -1,7 +1,34 @@
 package net.sourcekick.service.meta.conference
 
-class ConferenceRoutesSpec {
+import java.util.UUID
 
-  // TODO priority 2 write Akka HTTP route test
+import akka.actor.ActorSystem
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.testkit.{RouteTestTimeout, ScalatestRouteTest}
+import org.scalatest.{Matchers, WordSpec}
+
+import scala.concurrent.duration.DurationInt
+
+class ConferenceRoutesSpec extends WordSpec with Matchers with ScalatestRouteTest {
+
+  implicit def default(implicit system: ActorSystem): RouteTestTimeout = RouteTestTimeout(new DurationInt(5).second)
+
+  private def conferencesPath(conferenceUuid: String) = s"/api/v1/conferences/$conferenceUuid"
+
+  private val globalRoutes: GlobalRoutes =
+    new GlobalRoutes(scala.concurrent.ExecutionContext.Implicits.global, materializer)
+
+  private val sealedEntryPoint = Route.seal(globalRoutes.entryPoint)
+
+  // ----- Spec -----
+  "The PointRoutes" must {
+
+    val conferenceUuid = UUID.randomUUID().toString
+
+    "create get update and get a Conference" in {
+      ???
+    }
+
+  }
 
 }
